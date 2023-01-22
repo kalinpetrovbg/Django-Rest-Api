e7df129b9edc4dbf8f0ff3e5b0c98111149d4a16
Kalin <kalinpetrovbg@gmail.com>
2023-01-22 21:13:44 +0200

    hiding the password field from the response

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,13 @@
+"""Serializers for the user API view."""
 from rest_framework import serializers
 
-# from users.models import HackUser
 from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
+
     class Meta:
         model = get_user_model()
         fields = "__all__"
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
